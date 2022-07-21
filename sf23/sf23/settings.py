7c872ec7f0c1626fa270a6816ca7ec8e783b1811
Aayush Neupane <neupane0403@gmail.com>
2022-07-21 22:26:01 +0545

    add static root, csrf allow origin, and allowed hosts

--- sf23/sf23/settings.py
+++ sf23/sf23/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i(hwj_+j8+(&bzc&gm_db6c6xp24-xwquze)^*()=xlvd$p*j*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -115,10 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-
+STATIC_ROOT='/var/www/static/'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ["http://locus3002.eastus.cloudapp.azure.com"]
